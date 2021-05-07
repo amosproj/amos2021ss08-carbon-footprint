576fb20225ab7120f1fd7c9d0887ccd90442eeee
Martin Wagner <martin.wagner@fau.de>
2021-05-07 17:39:15 +0200

    Adding Documentation

--- frontend/src/interface/projectInterface.js
+++ frontend/src/interface/projectInterface.js
@@ -2,6 +2,19 @@ import logo from 'assets/logo/LogoCarbonteam.png'
 import logo_1 from 'assets/dummyImages/Image_1.PNG'
 import logo_2 from 'assets/dummyImages/Image_2.PNG'
 
+/**
+ * should get all the Products from the backend (soon)
+ * @returns 
+ */
+export function getProducts() {
+    return ([logo,logo_1,logo_2,logo_1,logo_2,logo_1]);
+}
+
+/**
+ * should get all the Models for one Product
+ * @param projectID the id of the Project to get the models for
+ * @returns 
+ */
 export function getModels() {
     return (  [
                             {
@@ -22,8 +35,3 @@ export function getModels() {
  
                         ]);
 }
-
-export function getProducts() {
-    return ([logo,logo_1,logo_2,logo_1,logo_2,logo_1]);
-}
-
