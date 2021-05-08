a6b60ab3237e498448954027379e963746cad76b
Martin Wagner <martin.wagner@fau.de>
2021-05-08 11:35:22 +0200

    Documentation

--- frontend/src/routes/scenarios/productGrid/DropdownButton.js
+++ frontend/src/routes/scenarios/productGrid/DropdownButton.js
@@ -3,7 +3,8 @@ import { getModels } from 'interface/projectInterface';
 import Select from 'react-select'
 import {PrivateSectionContext} from 'resources/PrivateSectionContext';
 
-//TODO: When a model is selected the user should be directed to the Details Page
+//TODO: When a model is selected, setSelectedProducts should be called with the selected product and model Id, 
+//      then the user should be directed to the Details Page
 //TODO: When only one model is available, => How to do that then? Should a click on the image link in that case?
 
 /**
@@ -22,11 +23,14 @@ function DropdownButton() {
             options={getModels()}
             onChange={(props) => {
                 console.log(props);
-                const newSelectedProducts =
-                {
+                const newSelectedProducts = 
+                [
+                {   
                     productID: 'abiut-08fbw-asdhi',
-                    productName: 'Ich bin ein TEST'
-                }
+                    productName: 'Ich bin ein TEST',
+                    modelID: 'efba-9sdfs-hallo',
+                    modelName: 'TestModelName'
+                }]
                 setSelectedProducts(newSelectedProducts);
             }} // props is the currently selected item.
         />
