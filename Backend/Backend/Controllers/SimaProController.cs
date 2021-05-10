989513392cf8d366975ac97887ffe86c3f1dd2e0
Martin Dürsch <Martin.duersch@acontech.de>
2021-05-10 17:19:01 +0200

    Backend initial commit

---
+++ Backend/Backend/Controllers/SimaProController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Backend.Services;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SimaProController : ControllerBase
+    {
+        SimaProQueryService _simaProQueryService;
+
+        public SimaProController(SimaProQueryService simaProQueryService)
+        {
+            _simaProQueryService = simaProQueryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTest()
+        {
+            await _simaProQueryService.TestAsync();
+            return new OkObjectResult("this ok");  
+        }
+    }
+}
---
+++ Backend/Backend/Security/SimaProLoginDelegatingHandler.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Backend.Security
+{
+    public class SimaProLoginDelegatingHandler : DelegatingHandler
+    {
+        private const string _loginPath = "token";
+        private readonly string baseUrl;
+        private readonly string simaProUser;
+        private readonly string simaProPassword;
+
+        private string accessToken = "";
+
+        public SimaProLoginDelegatingHandler(IConfiguration configuration)
+        {
+            baseUrl = Environment.GetEnvironmentVariable("BaseUrl") ?? configuration["BaseUrl"];
+            simaProUser = Environment.GetEnvironmentVariable("Username") ?? configuration["Username"];
+            simaProPassword = Environment.GetEnvironmentVariable("Password") ?? configuration["Password"];
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+       CancellationToken cancellationToken)
+        {
+            request.Headers.Add("Authenticate", accessToken);
+
+            var loginPath = baseUrl + _loginPath;
+            var loginRequest = new HttpRequestMessage(new HttpMethod("POST"), loginPath) {
+                Content = new StringContent(JsonConvert.SerializeObject(
+                    new {
+                        email = simaProUser,
+                        password = simaProPassword
+                    }), Encoding.UTF8, "application/json")
+            };
+            //loginRequest.Headers.Clear();
+
+            var loginResponse = await base.SendAsync(loginRequest, cancellationToken);
+
+            return loginResponse;
+        }
+    }
+}
