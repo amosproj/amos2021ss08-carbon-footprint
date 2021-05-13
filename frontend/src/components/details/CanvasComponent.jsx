f8f568814c7a0b54c6499c9bee53a7247acca586
parham gandomkar <parham.gandomkar@gmail.com>
2021-05-13 11:36:58 +0200

    adding seperation pannel between search and result components deleting css file making pie chart diagram compatible for small screen sizes

--- frontend/src/components/details/CanvasComponent.jsx
+++ frontend/src/components/details/CanvasComponent.jsx
@@ -8,34 +8,30 @@ such as diagrams and tables
 import React, { Component } from 'react';
 import PieChart from './PieChartComponent';
 import ColumnChart from './ColumnChartComponent';
-import Table from './TableComponent'
-import './canvas.css';
-import {Container, Row, Col } from 'react-grid-system';
-
+import Table from './TableComponent';
+import { Container, Row, Col } from 'react-grid-system';
 
 class CanvasPage extends Component {
     render() {
         return (
             <Container fluid={true}>
-            <Row>
-                <Col xs={5}>
-                    <h3> Material Composition </h3>
-                    <PieChart></PieChart>
-                </Col>
-                <Col xs={5}>
-                    <h3> Results of the impact assessment </h3>
-                    <ColumnChart></ColumnChart>
-                </Col>
-            </Row>
-            <Row>
-            <Col xs={10}>
-                <h3> Impact categories </h3>
-                <Table></Table>
-            </Col>
-            </Row>
-            
-        </Container>
-            
+                <Row>
+                    <Col xs={5}>
+                        <h3> Material Composition </h3>
+                        <PieChart></PieChart>
+                    </Col>
+                    <Col xs={5}>
+                        <h3> Results of the impact assessment </h3>
+                        <ColumnChart></ColumnChart>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={10}>
+                        <h3> Impact categories </h3>
+                        <Table></Table>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
--- frontend/src/components/details/DetailsComponent.js
+++ frontend/src/components/details/DetailsComponent.js
@@ -1,24 +1,27 @@
 /**
- * the main component for detail page which includes  
- * canvas page and variable drop down list  
- * 
- * @author parham, 08.05  
+ * the main component for detail page which includes
+ * canvas page and variable drop down list
+ *
+ * @author parham, 08.05
  */
 
-import React, {Component, useContext} from 'react';
+import React, { Component, useContext } from 'react';
 import Canvas from './CanvasComponent';
 import SelectVariable from './SelectVariableComponent';
-import {PrivateSectionContext} from "hooks/PrivateSectionContext";
+import { PrivateSectionContext } from 'hooks/PrivateSectionContext';
 
 function DetailInfo() {
-    const [ selectedProducts, setSelectedProducts ] = useContext(PrivateSectionContext);
-        return (
-            <React.Fragment>
-                <h2>The chosen Model is {selectedProducts[0].modelName}</h2>
-                <SelectVariable></SelectVariable>
-                <Canvas></Canvas>
-            </React.Fragment>
-        );
+    const [selectedProducts, setSelectedProducts] = useContext(PrivateSectionContext);
+    return (
+        <React.Fragment>
+            <h2>The chosen Model is {selectedProducts[0].modelName}</h2>
+            <SelectVariable></SelectVariable>
+            <div class='w3-panel w3-2019-bluestone w3-padding-16'>
+                <h2 class='w3-opacity'>Canvas page</h2>
+            </div>
+            <Canvas></Canvas>
+        </React.Fragment>
+    );
 }
 
 export default DetailInfo;
