6b56959e39c5b34da293b93a7712f35ba5647f80
Martin Wagner <martin.wagner@fau.de>
2021-05-18 21:33:42 +0200

    Alignment fixes
    Co-authored-by: Irem Toroslu <irem.toroslu@fau.de>

--- frontend/src/components/details/CompareCanvas.jsx
+++ frontend/src/components/details/CompareCanvas.jsx
@@ -5,64 +5,79 @@ import Table from './TableComponent';
 import { Container, Row, Col } from 'react-grid-system';
 import theme from 'resources/theme';
 /**
- * This component displays the difference/comparison between the dashboards of two different type of variables of the same model 
+ * This component displays the difference/comparison between the dashboards of two different type of variables of the same model
  * will be compared when clicking on the "comparison" button.
- * 
- * @returns the dasboard comparison 
+ *
+ * @returns the dasboard comparison
  * @author Parham Gandomkar, Irem Toroslu
- * 
- * 
+ *
+ *
  */
 
 const CompareCanvas = () => {
     return (
         <Container fluid={true}>
             <Row>
-                <Col xs={5}>
+                <Col xs={5} style={{ marginRight: 150 }}>
                     <h3
-                    style={{
-                        fontSize: theme.typography.subtitle.fontSize,
-                        fontWeight: theme.typography.subtitle.fontWeight,
-                        lineHeight: theme.typography.subtitle.lineHeight,
-                        letterSpacing: theme.typography.subtitle.letterSpacing,
-                        marginLeft:120
-                    }}> Material Composition </h3>
+                        style={{
+                            fontSize: theme.typography.subtitle.fontSize,
+                            fontWeight: theme.typography.subtitle.fontWeight,
+                            lineHeight: theme.typography.subtitle.lineHeight,
+                            letterSpacing: theme.typography.subtitle.letterSpacing,
+                            marginLeft: 120
+                        }}
+                    >
+                        {' '}
+                        Material Composition{' '}
+                    </h3>
                     <PieChart></PieChart>
                 </Col>
-                <Col xs={5} style={{marginLeft:150}}>
+                <Col xs={5}>
                     <h3
-                    style={{
-                        fontSize: theme.typography.subtitle.fontSize,
-                        fontWeight: theme.typography.subtitle.fontWeight,
-                        lineHeight: theme.typography.subtitle.lineHeight,
-                        letterSpacing: theme.typography.subtitle.letterSpacing,
-                        marginLeft:120
-                    }}
-                    > Material Composition </h3>
+                        style={{
+                            fontSize: theme.typography.subtitle.fontSize,
+                            fontWeight: theme.typography.subtitle.fontWeight,
+                            lineHeight: theme.typography.subtitle.lineHeight,
+                            letterSpacing: theme.typography.subtitle.letterSpacing,
+                            marginLeft: 120
+                        }}
+                    >
+                        {' '}
+                        Material Composition{' '}
+                    </h3>
                     <PieChart></PieChart>
                 </Col>
             </Row>
             <Row>
-                <Col xs={5}>
+                <Col xs={5} style={{ marginRight: 150 }}>
                     <h3
-                    style={{
-                        fontSize: theme.typography.subtitle.fontSize,
-                        fontWeight: theme.typography.subtitle.fontWeight,
-                        lineHeight: theme.typography.subtitle.lineHeight,
-                        letterSpacing: theme.typography.subtitle.letterSpacing,
-                        marginLeft:80
-                    }}> Results of the impact assessment </h3>
+                        style={{
+                            fontSize: theme.typography.subtitle.fontSize,
+                            fontWeight: theme.typography.subtitle.fontWeight,
+                            lineHeight: theme.typography.subtitle.lineHeight,
+                            letterSpacing: theme.typography.subtitle.letterSpacing,
+                            marginLeft: 80
+                        }}
+                    >
+                        {' '}
+                        Results of the impact assessment{' '}
+                    </h3>
                     <ColumnChart></ColumnChart>
                 </Col>
-                <Col xs={5} style={{marginLeft:150}}>
+                <Col xs={5}>
                     <h3
-                    style={{
-                        fontSize: theme.typography.subtitle.fontSize,
-                        fontWeight: theme.typography.subtitle.fontWeight,
-                        lineHeight: theme.typography.subtitle.lineHeight,
-                        letterSpacing: theme.typography.subtitle.letterSpacing,
-                        marginLeft:80
-                    }}> Results of the impact assessment </h3>
+                        style={{
+                            fontSize: theme.typography.subtitle.fontSize,
+                            fontWeight: theme.typography.subtitle.fontWeight,
+                            lineHeight: theme.typography.subtitle.lineHeight,
+                            letterSpacing: theme.typography.subtitle.letterSpacing,
+                            marginLeft: 80
+                        }}
+                    >
+                        {' '}
+                        Results of the impact assessment{' '}
+                    </h3>
                     <ColumnChart></ColumnChart>
                 </Col>
             </Row>
@@ -70,26 +85,35 @@ const CompareCanvas = () => {
             <Row>
                 <Col xs={10}>
                     <h3
-                    style={{
-                        fontSize: theme.typography.subtitle.fontSize,
-                        fontWeight: theme.typography.subtitle.fontWeight,
-                        lineHeight: theme.typography.subtitle.lineHeight,
-                        letterSpacing: theme.typography.subtitle.letterSpacing,
-
-                    }}> Impact categories </h3>
+                        style={{
+                            fontSize: theme.typography.subtitle.fontSize,
+                            fontWeight: theme.typography.subtitle.fontWeight,
+                            lineHeight: theme.typography.subtitle.lineHeight,
+                            letterSpacing: theme.typography.subtitle.letterSpacing
+                        }}
+                    >
+                        {' '}
+                        Impact categories{' '}
+                    </h3>
                     <Table></Table>
                 </Col>
             </Row>
             <Row className='w3-margin-top'>
                 <Col xs={10}>
                     <h3
-                    style={{
-                        fontSize: theme.typography.subtitle.fontSize,
-                        fontWeight: theme.typography.subtitle.fontWeight,
-                        lineHeight: theme.typography.subtitle.lineHeight,
-                        letterSpacing: theme.typography.subtitle.letterSpacing,
-                    }}> Impact categories </h3>
-                    <div style={{marginBottom:30}}><Table></Table></div>
+                        style={{
+                            fontSize: theme.typography.subtitle.fontSize,
+                            fontWeight: theme.typography.subtitle.fontWeight,
+                            lineHeight: theme.typography.subtitle.lineHeight,
+                            letterSpacing: theme.typography.subtitle.letterSpacing
+                        }}
+                    >
+                        {' '}
+                        Impact categories{' '}
+                    </h3>
+                    <div style={{ marginBottom: 30 }}>
+                        <Table></Table>
+                    </div>
                 </Col>
             </Row>
         </Container>
