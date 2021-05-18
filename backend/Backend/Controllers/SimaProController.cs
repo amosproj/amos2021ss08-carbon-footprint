086f92cb03082603f8282567489776ffb61a2e39
Martin Dürsch <Martin.duersch@acontech.de>
2021-05-18 18:13:16 +0200

    Implemented Proxy

--- backend/Backend/Controllers/SimaProController.cs
+++ backend/Backend/Controllers/SimaProController.cs
@@ -5,56 +5,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-using Backend.Services;
 using System.Net.Http;
 using Microsoft.Extensions.Primitives;
+using AspNetCore.Proxy;
+using AspNetCore.Proxy.Options;
 
 namespace Backend.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]/api")]
     [ApiController]
     public class SimaProController : ControllerBase
     {
-        SimaProQueryService _simaProQueryService;
+        private HttpProxyOptions httpProxyOptions = HttpProxyOptionsBuilder.Instance
+            .WithHttpClientName("SimaProClient")
+            .Build();
 
-        public SimaProController(SimaProQueryService simaProQueryService)
+        public SimaProController()
         {
-            _simaProQueryService = simaProQueryService;
         }
 
-        [HttpGet]
-        public async Task<IActionResult> GetTest()
+        [Route("{**rest}")]
+        public Task ProxyCatchAll(string rest)
         {
-            var response = await _simaProQueryService.TestAsync();
-            this.HttpContext.Response.RegisterForDispose(response);
-            return new HttpResponseMessageResult(response);
+            // If you don't need the query string, then you can remove this.
+            var queryString = this.Request.QueryString.Value;
+            return this.HttpProxyAsync($"{rest}{queryString}", httpProxyOptions);
         }
 
-        //copy paste from stackoverflow to build the new response from the response the backend got from the api
-        public class HttpResponseMessageResult : IActionResult
-        {
-            private readonly HttpResponseMessage _responseMessage;
-
-            public HttpResponseMessageResult(HttpResponseMessage responseMessage)
-            {
-                _responseMessage = responseMessage; // could add throw if null
-            }
-
-            public async Task ExecuteResultAsync(ActionContext context)
-            {
-                context.HttpContext.Response.StatusCode = (int)_responseMessage.StatusCode;
-
-                foreach (var header in _responseMessage.Headers)
-                {
-                    context.HttpContext.Response.Headers.TryAdd(header.Key, new StringValues(header.Value.ToArray()));
-                }
-
-                using (var stream = await _responseMessage.Content.ReadAsStreamAsync())
-                {
-                    await stream.CopyToAsync(context.HttpContext.Response.Body);
-                    await context.HttpContext.Response.Body.FlushAsync();
-                }
-            }
-        }
     }
 }
--- backend/Backend/Startup.cs
+++ backend/Backend/Startup.cs
@@ -1,7 +1,8 @@
+using AspNetCore.Proxy;
 using Backend.Security;
-using Backend.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -35,14 +36,15 @@ namespace Backend
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Backend", Version = "v1" });
             });
 
+            services.AddProxies();
+
             services.AddTransient<SimaProLoginDelegatingHandler>();
-            services.AddHttpClient<SimaProQueryService>(
-                client =>
-                {
-                    var baseUrl = Environment.GetEnvironmentVariable("BaseUrl") ?? Configuration["BaseUrl"];
-                    client.BaseAddress = new Uri(baseUrl);
-                    //client.Timeout = TimeSpan.FromSeconds(30);
-                })
+            services.AddHttpClient("SimaProClient",
+                 client => {
+                     var baseUrl = Environment.GetEnvironmentVariable("BaseUrl") ?? Configuration["BaseUrl"];
+                     client.BaseAddress = new Uri(baseUrl);
+                     //client.Timeout = TimeSpan.FromSeconds(30);
+                 })
                 .AddHttpMessageHandler<SimaProLoginDelegatingHandler>();
         }
 
@@ -52,8 +54,8 @@ namespace Backend
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Backend v1"));
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Backend v1"));
             }
 
             app.UseHttpsRedirection();
@@ -67,5 +69,6 @@ namespace Backend
                 endpoints.MapControllers();
             });
         }
+
     }
 }
