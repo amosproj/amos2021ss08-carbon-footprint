e63f33640df189b06126092021003ce029942a05
Julian <63585974+ScoutAtlas@users.noreply.github.com>
2021-05-21 15:52:15 +0200

    Created working "Generate PDF" button
    Inserted button into PanelComponent
    Create exportPDF function in PdfComponent
    Inserted dummy data for testing purpose

--- frontend/src/components/details/PanelComponent.jsx
+++ frontend/src/components/details/PanelComponent.jsx
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import theme from 'resources/theme';
+import ExportPDF from "./PdfComponent";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+const pdfConverter = require("jspdf")
 
 /**
  * a divider Pannel for seperating search compoents and result components
  * and also providing the comparison feature by compare button
  *
- * @author Parham Gandomkar, Irem Toroslu
+ * @author Parham Gandomkar, Irem Toroslu, Julian Oelhaf
  */
 
 const PanelComponent = (props) => {
     return (
-        <div className='w3-panel w3-padding-small' style={{backgroundColor:'#466e78',marginLeft:15}}>
+        <div className='w3-panel w3-padding-small' style={{ backgroundColor: '#466e78', marginLeft: 15 }}>
             <div className='w3-row'>
                 <div className='w3-col s9'>
-                    <h2 className='w3-opacity' style={{color:'white',fontSize:theme.typography.subtitle.fontSize,fontWeight:theme.typography.subtitle.fontWeight,lineHeight:theme.typography.subtitle.lineHeight,letterSpacing:theme.typography.subtitle.letterSpacing,marginTop:6}}>Canvas page</h2>
+                    <h2 className='w3-opacity' style={{ color: 'white', fontSize: theme.typography.subtitle.fontSize, fontWeight: theme.typography.subtitle.fontWeight, lineHeight: theme.typography.subtitle.lineHeight, letterSpacing: theme.typography.subtitle.letterSpacing, marginTop: 6 }}>Canvas page</h2>
                 </div>
                 <div className='w3-col s3  w3-center'>
                     {/* calling the compare button by using props which its parrent has provided*/}
@@ -23,9 +27,22 @@ const PanelComponent = (props) => {
                         className='w3-button w3-border'
                         disabled={props.loadComparePage}
                     >
-                        <b
-                        style={{color:'white' ,fontSize:theme.typography.buttonSendtitle.fontSize,fontWeight:theme.typography.buttonSendtitle.fontWeight,lineHeight:theme.typography.buttonSendtitle.lineHeight,letterSpacing:theme.typography.buttonSendtitle.letterSpacing}}>Compare</b>
+                        <b style={{ color: 'white', fontSize: theme.typography.buttonSendtitle.fontSize, fontWeight: theme.typography.buttonSendtitle.fontWeight, lineHeight: theme.typography.buttonSendtitle.lineHeight, letterSpacing: theme.typography.buttonSendtitle.letterSpacing }}>Compare</b>
                     </button>
+                    
+                    {/* Space to seperate buttons, dont get how the div works to put buttons next to each other*/}
+                    {" "} 
+
+                    {/* button: create pdf report by calling ExportPDF from PdfComponent*/}
+                    <button
+                        onClick={() => ExportPDF()}
+                        className='w3-button w3-border'
+                    >
+                        <b style={{ color: 'white', fontSize: theme.typography.buttonSendtitle.fontSize, fontWeight: theme.typography.buttonSendtitle.fontWeight, lineHeight: theme.typography.buttonSendtitle.lineHeight, letterSpacing: theme.typography.buttonSendtitle.letterSpacing }}>Create PDF Report</b>
+                    </button>
+                </div>
+                <div className='w3-col s3  w3-center'>
+                    
                 </div>
             </div>
         </div>
---
+++ frontend/src/components/details/PdfComponent.js
@@ -0,0 +1,30 @@
+import jsPDF from "jspdf";
+
+function exportPDF(props){
+    const unit = "pt";
+    const size = "A4"; // Use A1, A2, A3 or A4
+    const orientation = "portrait"; // portrait or landscape
+
+    const marginLeft = 40;
+    const doc = new jsPDF(orientation, unit, size);
+
+    doc.setFontSize(15);
+
+    const title = "My Awesome Report";
+    const headers = [["NAME", "PROFESSION"]];
+    const data = [["Julian", "Software Developer"],["Dummy", "Boss"] ];
+
+
+    let content = {
+      startY: 50,
+      head: headers,
+      body: data
+    };
+
+    doc.text(title, marginLeft, 40);
+    doc.autoTable(content);
+    //Output
+    doc.save("report.pdf");
+};
+
+export default exportPDF;
