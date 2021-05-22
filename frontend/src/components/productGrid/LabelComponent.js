c2ab389c915ec6d2288b5cdb62ca3d1eb99e0dc0
Martin Wagner <martin.wagner@fau.de>
2021-05-22 11:38:23 +0200

    Initial Setup of Jest for Test(ing)
    Regarding #55

--- frontend/src/components/productGrid/LabelComponent.js
+++ frontend/src/components/productGrid/LabelComponent.js
@@ -14,7 +14,7 @@ function LabelComponent({ productName }) {
                 fontSize: theme.typography.textcontent.fontSize,
                 fontWeight: theme.typography.textcontent.fontWeight
             }}
-            class='w3-container w3-center w3-lightgrey w3-margin-bottom:2em  w3-margin-top'
+            className='w3-container w3-center w3-lightgrey w3-margin-bottom:2em  w3-margin-top'
         >
             {productName}
         </span>
