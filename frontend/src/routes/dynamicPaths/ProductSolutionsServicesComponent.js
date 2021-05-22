7ec67f156a9b5d93a130b4392cab234b0c607875
Irem Toroslu <irem.toroslu@fau.de>
2021-05-22 12:22:28 +0200

    fixed the row and column problem regarding the privateroutes component

--- frontend/src/routes/dynamicPaths/ProductSolutionsServicesComponent.js
+++ frontend/src/routes/dynamicPaths/ProductSolutionsServicesComponent.js
@@ -53,10 +53,11 @@ export default function ProductSolutionsServicesComponent() {
                 {/* {type === 'products' && <div >All the products<div className={classes.textcontent}>  This is where all the products of the selected category could be shown.</div></div>}
       {type === 'solutions' && <div className={classes.textcontent}>This is where all the solutions of the selected category could be shown.</div>}
       {type === 'services' && <div className={classes.textcontent}>This is where all the services of the selected category could be shown.</div>} */}
-            </Row>
+
             <div style={{ marginLeft: 15 }}>
                 <ProductGridComponent />
             </div>
+            </Row>
         </Column>
     );
 }
