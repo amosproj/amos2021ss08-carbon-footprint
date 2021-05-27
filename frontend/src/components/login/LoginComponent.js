df59e06004c8ab23e8c702e36df04b5b8364af14
Martin Wagner <martin.wagner@fau.de>
2021-05-27 22:01:08 +0200

    Outsourced Styling to a css file
    Regarding #10
    
    Co-authored-by: Mani Anand <mani.anand@fau.de>

--- frontend/src/components/login/LoginComponent.js
+++ frontend/src/components/login/LoginComponent.js
@@ -3,34 +3,43 @@
  * @author Martin Wagner
  * */
 
-import style from "./Loginstyle"
-function LoginComponent( ) {
+import './Loginstyle.css';
 
-return (
-<>
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <style dangerouslySetInnerHTML={{__html: "\nbody {font-family: Arial, Helvetica, sans-serif;}\nform {border: 3px solid #f1f1f1;}\n\ninput[type=text], input[type=password] {\n  width: 100%;\n  padding: 12px 20px;\n  margin: 8px 0;\n  display: inline-block;\n  border: 1px solid #ccc;\n  box-sizing: border-box;\n}\n\nbutton {\n  background-color: #04AA6D;\n  color: white;\n  padding: 14px 20px;\n  margin: 8px 0;\n  border: none;\n  cursor: pointer;\n  width: 100%;\n}\n\nbutton:hover {\n  opacity: 0.8;\n}\n\n.cancelbtn {\n  width: auto;\n  padding: 10px 18px;\n  background-color: #f44336;\n}\n\n.imgcontainer {\n  text-align: center;\n  margin: 24px 0 12px 0;\n}\n\nimg.avatar {\n  width: 40%;\n  border-radius: 50%;\n}\n\n.container {\n  padding: 16px;\n}\n\nspan.psw {\n  float: right;\n  padding-top: 16px;\n}\n\n/* Change styles for span and cancel button on extra small screens */\n@media screen and (max-width: 300px) {\n  span.psw {\n     display: block;\n     float: none;\n  }\n  .cancelbtn {\n     width: 100%;\n  }\n}\n" }} />
-  <h2>Login Form</h2>
-  <form action="/action_page.php" method="post">
-    <div className="imgcontainer">
-      <img src="img_avatar2.png" alt="Avatar" className="avatar" />
-    </div>
-    <div className="container">
-      <label htmlFor="uname"><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" required />
-      <label htmlFor="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required />
-      <button type="submit">Login</button>
-      <label>
-        <input type="checkbox" defaultChecked="checked" name="remember" /> Remember me
-      </label>
-    </div>
-    <div className="container" style={{backgroundColor: '#f1f1f1'}}>
-      <button type="button" className="cancelbtn">Cancel</button>
-      <span className="psw">Forgot <a href="#">password?</a></span>
-    </div>
-  </form>
-</>
-)}
+function LoginComponent() {
+    return (
+        <>
+            <meta name='viewport' content='width=device-width, initial-scale=1' />
+            <h2>Login Form</h2>
+            <form action='/action_page.php' method='post'>
+                <div className='imgcontainer'>
+                    <img src='img_avatar2.png' alt='Avatar' className='avatar' />
+                </div>
+                <div className='container'>
+                    <label htmlFor='uname'>
+                        <b>Username</b>
+                    </label>
+                    <input type='text' placeholder='Enter Username' name='uname' required />
+                    <label htmlFor='psw'>
+                        <b>Password</b>
+                    </label>
+                    <input type='password' placeholder='Enter Password' name='psw' required />
+                    <button type='submit'>Login</button>
+                    <label>
+                        <input type='checkbox' defaultChecked='checked' name='remember' /> Remember
+                        me
+                    </label>
+                </div>
+                <div className='container' style={{ backgroundColor: '#f1f1f1' }}>
+                    <button type='button' className='cancelbtn'>
+                        Cancel
+                    </button>
+                    <span className='psw'>
+                        Forgot <a href='#'>password?</a>
+                    </span>
+                </div>
+            </form>
+        </>
+    );
+}
 
 export default LoginComponent;
