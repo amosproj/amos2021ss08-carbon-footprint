8f2f55702fe4d57c19605fa6f0c71d21173cd074
Martin Wagner <martin.wagner@fau.de>
2021-05-28 13:20:01 +0200

    Rounded Corners

--- frontend/src/components/login/LoginComponent.js
+++ frontend/src/components/login/LoginComponent.js
@@ -10,42 +10,46 @@ import slugs from 'resources/slugs';
  */
 function LoginComponent() {
     return (
-        <div
-            className='w3-container w3-padding-24 w3-auto'
-            style={{ backgroundColor: uniformStyle.color.primaryBackgroundColor }}
-        >
-            <meta name='viewport' content='width=device-width, initial-scale=1' />
-            {/* this form does not have any function other than relaying right now */}
-            <form action={slugs.categories} method='post'>
-                <div className='imgcontainer'>
-                    <img src={CarbonFootprintLogo} alt='Carbon Footprint' className='avatar' />
-                </div>
-                <div className='container'>
-                    <label htmlFor='uname'>
-                        <b>Username</b>
-                    </label>
-                    <input type='text' placeholder='Enter Username' name='uname' required />
-                    <label htmlFor='psw'>
-                        <b>Password</b>
-                    </label>
-                    <input type='password' placeholder='Enter Password' name='psw' required />
-                    <button type='submit'>Login</button>
-                    <label>
-                        <input type='checkbox' defaultChecked='checked' name='remember' /> Remember
-                        me
-                    </label>
-                </div>
-                <div
-                    className='container'
-                    style={{
-                        marginBottom: '10px',
-                        textAlign: 'right',
-                        backgroundColor: '#f1f1f1'
-                    }}
+        <div className='table'>
+            <div className='w3-padding-24 w3-auto'>
+                <meta name='viewport' content='width=device-width, initial-scale=1' />
+                {/* this form does not have any function other than relaying right now */}
+                <form
+                    action={slugs.categories}
+                    method='post'
+                    style={{ backgroundColor: uniformStyle.color.primaryBackgroundColor }}
                 >
-                    <a href={slugs.forgotPassword}>Forgot password?</a>
-                </div>
-            </form>
+                    <div className='imgcontainer'>
+                        <img src={CarbonFootprintLogo} alt='Carbon Footprint' className='avatar' />
+                    </div>
+                    <div className='container'>
+                        <label htmlFor='uname'>
+                            <b>Username</b>
+                        </label>
+                        <input type='text' placeholder='Enter Username' name='uname' required />
+                        <label htmlFor='psw'>
+                            <b>Password</b>
+                        </label>
+                        <input type='password' placeholder='Enter Password' name='psw' required />
+                        <button type='submit'>Login</button>
+                        <label>
+                            <input type='checkbox' defaultChecked='checked' name='remember' />{' '}
+                            Remember me
+                        </label>
+                        <div
+                            className='container'
+                            style={{
+                                marginTop: '10px',
+                                marginBottom: '10px',
+                                textAlign: 'right',
+                                backgroundColor: '#f1f1f1'
+                            }}
+                        >
+                            <a href={slugs.forgotPassword}>Forgot password?</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
