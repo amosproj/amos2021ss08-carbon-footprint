963fc97e073df72ad2ec85fede1149998bcffaaa
Irem Toroslu <irem.toroslu@fau.de>
2021-06-02 17:44:44 +0200

    Worked on the .css file
    reconfiguration of the header and title

--- frontend/src/components/sidebar/MenuItemComponent.js
+++ frontend/src/components/sidebar/MenuItemComponent.js
@@ -67,7 +67,7 @@ function MenuItemComponent({ children, icon: Icon, id, items = [], level = 1, on
         <Column key={id} className={classNameColumn}>
             <Row vertical='center' onClick={onItemClicked} className={classNameContainer}>
                 <Icon fill={iconColor} opacity={!isActive && '0.4'} />
-                <span className={classes.title}>{title}</span>
+                <span className="SideBarTitle">{title}</span>
             </Row>
             {isCollapsible && (
                 <CollapsibleContent expanded={isExpanded}>
