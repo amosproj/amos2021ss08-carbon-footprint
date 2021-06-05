61878e8960ffa806c510e83e212fc679741a6eca
Martin Wagner <wagnmart@gmail.com>
2021-06-05 17:41:11 +0200

    Revert "Implementation of #74"

--- frontend/src/components/details/CompareVariablesComponent.jsx
+++ frontend/src/components/details/CompareVariablesComponent.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import theme from 'resources/theme';
-import DropDownComponent from 'components/dropdown/DropDownComponent';
 
 /** 
 a drop down component for selecting variables 
@@ -16,18 +15,72 @@ const CompareVariablesComponent = (props) => {
                 <h4>Select variables to compare:</h4>
             </div>
             <div className='w3-col l3 m3 s3 w3-left'>
-                <DropDownComponent
-                    selectedVariable={state.selectedVariable}
-                    variables={state.variables}
-                    dropDownHandler={props.firstDropDownHandler}
-                />
+                <div className='w3-dropdown-hover '>
+                    <button
+                        className='w3-button w3-2018-sailor-blue'
+                        style={{
+                            fontSize: theme.typography.buttontitle.fontSize,
+                            fontWeight: theme.typography.buttontitle.fontWeight,
+                            lineHeight: theme.typography.buttontitle.lineHeight,
+                            letterSpacing: theme.typography.buttonSendtitle.letterSpacing
+                        }}
+                    >
+                        {state.selectedVariable}
+                    </button>
+                    <div
+                        className='w3-dropdown-content w3-bar-block w3-border'
+                        style={{
+                            fontSize: theme.typography.buttontitle.fontSize,
+                            fontWeight: theme.typography.buttontitle.fontWeight,
+                            lineHeight: theme.typography.buttontitle.lineHeight,
+                            letterSpacing: theme.typography.buttonSendtitle.letterSpacing
+                        }}
+                    >
+                        {state.variables.map((item) => (
+                            <button
+                                onClick={() => props.firstDropDownHandler(item.name)}
+                                className='w3-bar-item w3-button'
+                                key={item.id}
+                            >
+                                {item.name}
+                            </button>
+                        ))}
+                    </div>
+                </div>
             </div>
             <div className='w3-col l3 m3 s3 w3-left'>
-                <DropDownComponent
-                    selectedVariable={state.secondVariable}
-                    variables={state.variables}
-                    dropDownHandler={props.secondDropDownHandler}
-                />
+                <div className='w3-dropdown-hover'>
+                    <button
+                        className='w3-button w3-2018-sailor-blue'
+                        style={{
+                            fontSize: theme.typography.buttontitle.fontSize,
+                            fontWeight: theme.typography.buttontitle.fontWeight,
+                            lineHeight: theme.typography.buttontitle.lineHeight,
+                            letterSpacing: theme.typography.buttonSendtitle.letterSpacing
+                        }}
+                    >
+                        {state.secondVariable}
+                    </button>
+                    <div
+                        className='w3-dropdown-content w3-bar-block w3-border'
+                        style={{
+                            fontSize: theme.typography.buttontitle.fontSize,
+                            fontWeight: theme.typography.buttontitle.fontWeight,
+                            lineHeight: theme.typography.buttontitle.lineHeight,
+                            letterSpacing: theme.typography.buttonSendtitle.letterSpacing
+                        }}
+                    >
+                        {state.variables.map((item) => (
+                            <button
+                                onClick={() => props.secondDropDownHandler(item.name)}
+                                className='w3-bar-item w3-button'
+                                key={item.id}
+                            >
+                                {item.name}
+                            </button>
+                        ))}
+                    </div>
+                </div>
             </div>
             <div className='w3-col l2 m2 s1 w3-right'>
                 <button
@@ -47,6 +100,22 @@ const CompareVariablesComponent = (props) => {
                     </b>
                 </button>
             </div>
+            <button
+                onClick={props.submitHandler}
+                style={{ backgroundColor: theme.uniformStyle.color.sendButtonColor }}
+                className='w3-button w3-wide'
+            >
+                <b
+                    style={{
+                        fontSize: theme.typography.buttonSendtitle.fontSize,
+                        fontWeight: theme.typography.buttonSendtitle.fontWeight,
+                        letterSpacing: theme.typography.buttonSendtitle.letterSpacing,
+                        lineHeight: theme.typography.buttonSendtitle.lineHeight
+                    }}
+                >
+                    Send Request
+                </b>
+            </button>
         </div>
     );
 };
