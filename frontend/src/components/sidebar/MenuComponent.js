5867668a69209c28e4e04b1d26507eeeca78d367
Martin Wagner <martin.wagner@fau.de>
2021-06-07 14:56:57 +0200

    Documentation

--- frontend/src/components/sidebar/MenuComponent.js
+++ frontend/src/components/sidebar/MenuComponent.js
@@ -3,6 +3,7 @@ import { useTheme } from 'react-jss';
 import { slide as Menu } from 'react-burger-menu';
 
 const getMenuStyles = ({ theme }) => ({
+    /* Position and sizing of burger button */
     bmBurgerButton: {
         position: 'absolute',
         width: 26,
@@ -11,22 +12,31 @@ const getMenuStyles = ({ theme }) => ({
         top: 25,
         zIndex: 19
     },
+    /* Color/shape of burger icon bars */
     bmBurgerBars: {
         background: 'white'
     },
+    /* Color/shape of burger icon bars on hover*/
     bmBurgerBarsHover: {
         background: theme.color.darkRed
     },
+    /* Position and sizing of clickable cross button */
     bmCrossButton: {},
+    /* Color/shape of close button cross */
     bmCross: {
         background: theme.color.grayishBlue3
     },
+    /*
+Sidebar wrapper styles
+Note: Beware of modifying this element as it can break the animations - you should not need to touch it in most cases
+*/
     bmMenuWrap: {
         position: 'fixed',
         height: '100%',
         width: 200,
         zIndex: 30
     },
+    /* General sidebar styles */
     bmMenu: {
         background: theme.uniformStyle.color.secondaryBackgroundColor // left side bar backgroundcolor
     },
