a5faae342a2b59d6e366de67805cfbb4c2cf3d36
Martin Dürsch <Martin.duersch@acontech.de>
2021-06-13 03:49:30 +0200

    Added DocumentController

---
+++ backend/Backend/Controllers/DocumentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DocumentController : ControllerBase
+    {
+        [HttpPost("test")]
+        public IActionResult test()
+        {
+            return new OkResult();
+        }
+    }
+}
