4f9fa6c6e04be1557c3eb4c091ec7b1b0306931a
Mani <mani.anand@fau.de>
2021-06-18 22:08:37 +0200

    Configuring the category page
    Regarding task  #100

---
+++ frontend/src/components/productGrid/CategoryIndex.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './ProductGridIndex.css';
+import GenerationImage from 'assets/dummyImages/powerGeneration.jpg';
+import TransmissionImage from 'assets/dummyImages/powerTransmission.jpg';
+import IndustrialImage from 'assets/dummyImages/Industrialapplications.jpg';
+
+function CategoryIndex() {
+    return (
+        <div className='w3-row w3-padding '>
+            <div className='w3-col w3-padding-small w3-padding-top'>
+                <strong className='SubTitle'>Our Offerings</strong>
+                <p className='TextContent'>
+                    Meeting the growing global energy demand while transitioning to a more
+                    sustainable world requires a complex journey. But you're not without a partner.
+                    At Siemen's Carbon Visualization, we work closely with our customers to drive
+                    the energy transition, step by step, with our rich history of innovative
+                    technology,extensive energy experience and ambitious strategy to drive the
+                    decarbonization of global energy systems.
+                </p>
+            </div>
+            <div class='w3-row m6 32 w3-padding-small'>
+                <div class='w3-third imageTitle w3-animate-bottom'>
+                    <img src={GenerationImage} alt='Generation' />
+                    <div class='w3-container w3-center'>
+                        <p>Generation</p>
+                    </div>
+                </div>
+                <div class='w3-third imageTitle w3-animate-zoom'>
+                    <img src={IndustrialImage} alt='Industry' />
+                    <div class='w3-container w3-center'>
+                        <p>Industrial Application</p>
+                    </div>
+                </div>
+                <div class='w3-third imageTitle w3-animate-bottom'>
+                    <img src={TransmissionImage} alt='Transmission' />
+                    <div class='w3-container w3-center'>
+                        <p>Transmission</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CategoryIndex;
