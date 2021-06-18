5ea9bb99fe237424e3ef1119f9afd441c222d83b
Irem Toroslu <irem.toroslu@fau.de>
2021-06-18 11:00:51 +0200

    reconfiguration of navbar and close icons
    regarding the #101
    
    Co-authored-by: Julian Oelhaf <julian.oelhaf@posteo.de>

--- frontend/src/components/details/NavbarComponent.js
+++ frontend/src/components/details/NavbarComponent.js
@@ -22,37 +22,45 @@ const NavbarComponent = (props) => {
 
                 <Link to={{ pathname: slugs.details }} onClick={props.onExportClicked}>
                     <pdfbtn className='w3-col l3 m3 s4'>
-                        <i className='fa fa-file-pdf-o w3-margin-right' aria-hidden='true'></i>
+                        <i className='fa fa-file-pdf-o ' aria-hidden='true'></i>
                         Export Pdf
                     </pdfbtn>
                 </Link>
                 <Link to={{ pathname: slugs.details }} onClick={props.onCompareClick}>
-                    <addbtn className='w3-col l2 m2 s2 w3-right'>
+                    <addbtn className='w3-col l2 m2 s2'>
                         <i className='fa fa-fw fa-plus-circle' /> Add
                     </addbtn>
                 </Link>
             </div>
         );
     } else {
+
         return (
             <div className='navbar w3-row' vertical='center' horizontal='space-between'>
                 <b className='w3-col l7 m7 s6'>{props.scenarioName}</b>
                 <Link to={{ pathname: slugs.details }} onClick={props.onExportClicked}>
                     <pdfbtn className='w3-col l5 m5 s6 w3-right'>
-                        <i className='fa fa-file-pdf-o w3-margin-right' aria-hidden='true'></i>
+                        <i className='fa fa-file-pdf-o ' aria-hidden='true'></i>
                         Export Pdf
                     </pdfbtn>
                 </Link>
-                <Link to={{ pathname: slugs.details }} onClick={props.onExportClicked}>
+                
+
+                <Link to={{ pathname: slugs.details }}>
                     <closebtn className='w3-col l5 m5 s6 w3-right'>
-                        <i className='fa fa-times-circle-o w3-margin-right' aria-hidden='true'></i>
+                        <i className='fa fa-times-circle-o' aria-hidden='true'></i>
                         Close
                     </closebtn>
                 </Link>
                 
             </div>
+
         );
-    }
+
+        }
+
+
+        
 };
 
 export default NavbarComponent;
