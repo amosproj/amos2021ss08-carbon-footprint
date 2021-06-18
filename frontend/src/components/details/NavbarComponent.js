34fad88b2b9956b8457b324040f1d922aee21da5
Irem Toroslu <irem.toroslu@fau.de>
2021-06-18 09:17:08 +0200

    fixed the navbar componant
    regarding the #100

--- frontend/src/components/details/NavbarComponent.js
+++ frontend/src/components/details/NavbarComponent.js
@@ -13,11 +13,11 @@ const NavbarComponent = (props) => {
     const history = useHistory();
     if (!props.loadComparePage) {
         return (
-            <div className='navbar w3-row' vertical='center' horizontal='space-between'>
+            <div className='navbar' vertical='center' horizontal='space-between'>
                {/* used the history.goback() function to go one step backward where it stores the previous steps including the prevs stage of clicking items on the sidebar */}
                
                 <Link  onClick={() => history.goBack()}>
-                    <btn className='w3-col l1 m1 s1 w3-center' >
+                    <btn className='w3-col l3 m3 s4 w3-left' >
                         <i class='fa fa-chevron-left' aria-hidden='true' />
                     </btn>
                 </Link>
@@ -30,21 +30,26 @@ const NavbarComponent = (props) => {
                     </pdfbtn>
                 </Link>
                 <Link to={{ pathname: slugs.details }} onClick={props.onCompareClick}>
-                    <addbtn className='w3-col l2 m2 s2 w3-right'>
-                        <i className='fa fa-fw fa-plus-circle' /> Add
+                    <addbtn className='w3-col l3 m3 s4 w3-right'>
+                        <i className='fa fa-fw fa-plus-circle w3-margin-right' /> Add
                     </addbtn>
                 </Link>
             </div>
         );
     } else {
         return (
-            <div className='navbar w3-row' vertical='center' horizontal='space-between'>
+            <div className='navbar' vertical='center' horizontal='space-between'>
                 <b className='w3-col l7 m7 s6'>{props.scenarioName}</b>
                 <Link to={{ pathname: slugs.details }} onClick={props.onExportClicked}>
                     <pdfbtn className='w3-col l5 m5 s6 w3-right'>
                         <i className='fa fa-file-pdf-o w3-margin-right' aria-hidden='true'></i>
                         Export Pdf
                     </pdfbtn>
+                </Link>
+                <Link to={{ pathname: slugs.details }} onClick={props.onExportClicked}>
+                    <closebtn className='w3-col l5 m5 s6 w3-right'>
+                        <i className='fa fa-times-circle-o w3-margin-right' aria-hidden='true'></i>
+                    </closebtn>
                 </Link>
             </div>
         );
