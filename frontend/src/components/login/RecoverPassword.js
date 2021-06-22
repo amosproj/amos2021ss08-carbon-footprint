c93821cba78652f49e1f4200edf4a980c8aaec80
Mani <mani.anand@fau.de>
2021-06-22 16:51:22 +0200

    Configured the recover password page
    Regarding #11

---
+++ frontend/src/components/login/RecoverPassword.js
@@ -0,0 +1,56 @@
+import './RecoverPasswordStyle.css';
+import { useContext } from 'react';
+import slugs from 'resources/slugs';
+import { GlobalContext } from 'hooks/GlobalContext';
+import React from 'react';
+
+function RecoverPassword() {
+    const [, setState] = useContext(GlobalContext);
+
+    return (
+        <div className='flex-container-recover'>
+            <div className='w3-padding-24 w3-auto'>
+                <form
+                    className='recover'
+                    action={slugs.categories}
+                    onSubmit={() => setState({ userIsLoggedIn: true })}
+                >
+                    <div className='w3-container w3-large w3-center'>
+                        <div className='w3-center-align w3-padding-24'>
+                            <h1>Forgot Your Password ?</h1>
+                        </div>
+                    </div>
+
+                    <div className='recoverpassword-container'>
+                        <div className='w3-center-align w3-padding-16'>
+                            <p>
+                                Don't fret! Just type in your email and we will send you a code to
+                                reset the password
+                            </p>
+                        </div>
+                        <label htmlFor='uname'>
+                            <b>Your Email</b>
+                        </label>
+                        <input
+                            className='recover'
+                            type='text'
+                            placeholder='abc@siemens.com'
+                            name='uname'
+                            required
+                        />
+
+                        <button className='recover' type='submit'>
+                            Recover Password
+                        </button>
+
+                        <div className='recoverpassword-container'>
+                            <a href={slugs.login}> Back to SignIn </a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default RecoverPassword;
--- frontend/src/routes/PublicRoutes.js
+++ frontend/src/routes/PublicRoutes.js
@@ -8,13 +8,14 @@ import { Redirect, Route, Router, Switch, useHistory } from 'react-router-dom';
 import SLUGS from 'resources/slugs';
 import Registration from 'components/login/Registration.js';
 import LoginComponent from 'components/login/LoginComponent';
+import RecoverPassword from 'components/login/RecoverPassword';
 function PublicRoutes() {
     return (
         <Router history={useHistory()}>
             <Switch>
                 <Route exact path={SLUGS.login} component={LoginComponent} />
                 <Route path={SLUGS.signup} component={Registration} />
-                <Route path={SLUGS.forgotPassword} render={() => <div>forgotPassword</div>} />
+                <Route path={SLUGS.forgotPassword} component={RecoverPassword} />
                 <Route path={SLUGS.logout} component={LoginComponent} />
                 <Redirect to={SLUGS.login} />
             </Switch>
