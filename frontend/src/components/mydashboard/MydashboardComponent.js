38a9c3f19103befcb3989784343ff0fd4f99a4f8
Irem Toroslu <irem.toroslu@fau.de>
2021-06-24 22:27:36 +0200

    mydashboard page reconfigured
    regarding the #125
    
    Co-authored-by: Mani Anand <mani.anand@fau.de>

---
+++ frontend/src/components/mydashboard/MydashboardComponent.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Column, Row } from 'simple-flexbox';
+import { createUseStyles } from 'react-jss';
+import MiniCardComponent from 'components/cards/MiniCardComponent';
+import MydashboardItemComponent from './MydashboardItemComponent';
+import UnresolvedTicketsComponent from 'components/dashboard/UnresolvedTicketsComponent';
+
+const useStyles = createUseStyles({
+    cardsContainer: {
+        marginRight: -30,
+        marginTop: -30
+    },
+    cardRow: {
+        marginTop: 30,
+        '@media (max-width: 768px)': {
+            marginTop: 0
+        }
+    },
+    miniCardContainer: {
+        flexGrow: 1,
+        marginRight: 30,
+        '@media (max-width: 768px)': {
+            marginTop: 30,
+            maxWidth: 'none'
+        }
+    },
+    todayTrends: {
+        marginTop: 30
+    },
+    lastRow: {
+        marginTop: 30
+    },
+    unresolvedTickets: {
+        marginRight: 30,
+        '@media (max-width: 1024px)': {
+            marginRight: 0
+        }
+    },
+    tasks: {
+        marginTop: 0,
+        '@media (max-width: 1024px)': {
+            marginTop: 30
+        }
+    }
+});
+
+function DashboardComponent() {
+    const classes = useStyles();
+    return (
+        <Column>
+            <div className={classes.todayTrends}>
+                <MydashboardItemComponent />
+            </div>
+        </Column>
+    );
+}
+
+export default DashboardComponent;
