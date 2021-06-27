afcc25c63e24285a499edf1fb8573cc914c3e228
parham gandomkar <parham.gandomkar@gmail.com>
2021-06-28 01:37:10 +0200

    documenting and refactoring ui

--- frontend/src/interface/PdfExporter.js
+++ frontend/src/interface/PdfExporter.js
@@ -9,46 +9,62 @@ import FifthPDFPage1 from 'assets/pdfImages/fifthPage1.jpg';
 import FifthPDFPage2 from 'assets/pdfImages/fifthPage2.jpg';
 import SixthPDFPage from 'assets/pdfImages/sixthPage.jpg';
 
+/**
+ * generating pdf for the baseline scenario
+ *
+ * @author Parham Gandomkar
+ *
+ * @param div1 pie chart diagram
+ * @param div2 column diagram
+ * @param div3 table
+ */
 export function exportPdf(div1, div2, div3) {
     // converting html to an image and then exporting it by pdf
     html2canvas(div1).then((canvas1) => {
-        var imgData1 = canvas1.toDataURL('image/jpeg', 1);
-
         var pdf = new jsPDF('p', 'mm', 'a4');
         // getting width and height to set image to fit the page
         var pageWidth = pdf.internal.pageSize.getWidth();
         var pageHeight = pdf.internal.pageSize.getHeight();
 
+        // storing width and heigth of the pie chart diagram
         var imageWidth1 = canvas1.width;
         var imageHeight1 = canvas1.height;
 
+        // storing the ratio of the pie chart diagram
         var ratio1 =
             imageWidth1 / imageHeight1 >= pageWidth / pageHeight
                 ? pageWidth / imageWidth1
                 : pageHeight / imageHeight1;
 
         html2canvas(div2).then((canvas2) => {
-            var imgData2 = canvas2.toDataURL('image/jpeg', 1);
-
+            // storing width and heigth of the column diagram
             var imageWidth2 = canvas2.width;
             var imageHeight2 = canvas2.height;
 
+            // storing the ratio of the column diagram
             var ratio2 =
                 imageWidth2 / imageHeight2 >= pageWidth / pageHeight
                     ? pageWidth / imageWidth2
                     : pageHeight / imageHeight2;
 
             html2canvas(div3).then((canvas3) => {
+                // converting html2canvas object to image for sending it to server
+                var imgData1 = canvas1.toDataURL('image/jpeg', 1);
+                var imgData2 = canvas2.toDataURL('image/jpeg', 1);
                 var imgData3 = canvas3.toDataURL('image/jpeg', 1);
+                // sendPdfGeneratorRequest(imgdata3, imgdata3, imgData3);
 
+                // storing width and heigth of the table
                 var imageWidth3 = canvas3.width;
                 var imageHeight3 = canvas3.height;
 
+                // storing the ratio of the table
                 var ratio3 =
                     imageWidth3 / imageHeight3 >= pageWidth / pageHeight
                         ? pageWidth / imageWidth3
                         : pageHeight / imageHeight3;
 
+                // adding images to pdf
                 pdf.addImage(FirstPDFPage, 'JPEG', 0, 0, pageWidth, pageHeight);
                 pdf.addPage();
                 pdf.addImage(SecondPDFPage, 'JPEG', 0, 0, pageWidth, pageHeight);
@@ -66,8 +82,8 @@ export function exportPdf(div1, div2, div3) {
                     'JPEG',
                     pageWidth / 3,
                     pageHeight - imageHeight3 * ratio3,
-                    pageWidth / 2,
-                    pageHeight / 5
+                    0,
+                    pageHeight / 4
                 );
                 pdf.addPage();
                 pdf.addImage(
@@ -84,7 +100,7 @@ export function exportPdf(div1, div2, div3) {
                     0,
                     pageHeight - imageHeight2 * ratio2,
                     imageWidth2 * ratio2,
-                    imageHeight2 * ratio2
+                    imageHeight2 * ratio2 - 10
                 );
                 pdf.addPage();
                 pdf.addImage(FifthPDFPage1, 'JPEG', 0, 0, pageWidth, pageHeight / 5);
