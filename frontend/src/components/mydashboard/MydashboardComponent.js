25e5582d9bead0ff5a9d3f4213e3903de15cd033
Martin Wagner <martin.wagner@fau.de>
2021-07-05 11:17:16 +0200

    Cleaning up unused Code

--- frontend/src/components/mydashboard/MydashboardComponent.js
+++ frontend/src/components/mydashboard/MydashboardComponent.js
@@ -1,58 +1,8 @@
 import React from 'react';
-import { createUseStyles } from 'react-jss';
 import MydashboardItemComponent from './MydashboardItemComponent';
-import { Container, Col } from 'react-grid-system';
-
-const useStyles = createUseStyles({
-    cardsContainer: {
-        marginRight: -30,
-        marginTop: -30
-    },
-    cardRow: {
-        marginTop: 30,
-        '@media (max-width: 768px)': {
-            marginTop: 0
-        }
-    },
-    miniCardContainer: {
-        flexGrow: 1,
-        marginRight: 30,
-        '@media (max-width: 768px)': {
-            marginTop: 30,
-            maxWidth: 'none'
-        }
-    },
-    todayTrends: {
-        marginTop: 30
-    },
-    lastRow: {
-        marginTop: 30
-    },
-    unresolvedTickets: {
-        marginRight: 30,
-        '@media (max-width: 1024px)': {
-            marginRight: 0
-        }
-    },
-    tasks: {
-        marginTop: 0,
-        '@media (max-width: 1024px)': {
-            marginTop: 30
-        }
-    }
-});
 
 function DashboardComponent() {
-    const classes = useStyles();
-    return (
-        <Container>
-            <Col>
-                <div className={classes.todayTrends}>
-                    <MydashboardItemComponent />
-                </div>
-            </Col>
-        </Container>
-    );
+    return <MydashboardItemComponent />;
 }
 
 export default DashboardComponent;
